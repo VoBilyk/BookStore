5f5f0053da8aa116c9575a6c175fe329c5bcd0c7
Volodymyr <volodymyr_bilyk@outlook.com>
2018-08-20 00:33:52 +0300

    Revert "Remove"
    This reverts commit 3af42b43b2bb1401e6678e6f1fdc70adac3e5322.

---
+++ BookStore.ConsoleApp/Interfaces/IMenuVisualizer.cs
@@ -0,0 +1,45 @@
+﻿namespace BookStore.ConsoleApp.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Interface for visualizing menu
+    /// </summary>
+    public interface IMenuVisualizer
+    {
+        /// <summary>
+        /// Show collection as list
+        /// </summary>
+        /// <typeparam name="T">Generic type of collection</typeparam>
+        /// <param name="items">Collection which need to show</param>
+        void ShowCollection<T>(IList<T> items);
+
+        /// <summary>
+        /// Getting entered value
+        /// </summary>
+        /// <returns>Entered value</returns>
+        int ReadInt();
+
+        /// <summary>
+        /// Read int value with thresholds
+        /// </summary>
+        /// <param name="min">Min possible value</param>
+        /// <param name="max">Max possible value</param>
+        /// <returns>Entered value</returns>
+        int ReadInt(int min, int max);
+
+        /// <summary>
+        /// Display created menu
+        /// </summary>
+        void Display();
+
+        /// <summary>
+        /// Add new menu item
+        /// </summary>
+        /// <param name="option">Item name</param>
+        /// <param name="callback">Operating which need to execute when choose item</param>
+        /// <returns>Current IMenuVisualizer instance</returns>
+        IMenuVisualizer Add(string option, Action callback);
+    }
+}
